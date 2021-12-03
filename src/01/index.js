ee161f4fddbc07a5e52ffa6ab5461a56706b6e9f
Ilê Caian <ile.caian@gmail.com>
2021-12-02 23:17:32 -0300

    refactor: improve day 01

--- src/01/index.js
+++ src/01/index.js
@@ -1,37 +1,34 @@
 import { readFile } from '_utils/file'
 
+const STARTING_COUNTER = 0
+
+const getWindowSum = (measurements, index) =>
+  measurements
+    .slice(index, index + 3)
+    .reduce((sum, value) => parseInt(value) + sum, 0)
+
 const solve = (measurements) => {
-  let increaseTimes = -1 // first one will always increase
-  let previous = 0
-
-  measurements.forEach((measurement) => {
-    const actual = parseInt(measurement)
-    if (actual > previous) {
-      increaseTimes += 1
-    }
-
-    previous = actual
-  })
+  const increaseTimes = measurements
+    .slice(1)
+    .reduce(
+      (times, measurement, index) =>
+        parseInt(measurement) > parseInt(measurements[index])
+          ? times + 1
+          : times,
+      STARTING_COUNTER
+    )
 
   console.log('> result 1:', increaseTimes)
 
-  let windowIncreaseTimes = -1 // first one will always increase
-  previous = 0
-
-  measurements.forEach((measurement, i) => {
-    if (i < measurements.length - 3) {
-      const actual =
-        parseInt(measurement) +
-        parseInt(measurements[i + 1]) +
-        parseInt(measurements[i + 2])
-
-      if (actual > previous) {
-        windowIncreaseTimes += 1
-      }
-
-      previous = actual
-    }
-  })
+  const windowIncreaseTimes = measurements
+    .slice(2, -3)
+    .reduce(
+      (times, _measurement, i) =>
+        getWindowSum(measurements, i) > getWindowSum(measurements, i - 1)
+          ? times + 1
+          : times,
+      STARTING_COUNTER
+    )
 
   console.log('> result 2:', windowIncreaseTimes)
 }
