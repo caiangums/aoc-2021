f055725e9f309a9837242b86f0eed258c0763c54
Ilê Caian <ile.caian@gmail.com>
2021-12-13 20:45:54 -0300

    solve: day 09 part 2

--- src/09/index.js
+++ src/09/index.js
@@ -3,18 +3,19 @@ import { readFile } from '_utils/file'
 const toNumberArray = (a) => a.map((v) => parseInt(v))
 
 const sum = (a) => a.reduce((sum, v) => v + sum, 0)
+const multiplyArray = (a) => a.reduce((mult, v) => v * mult, 1)
 
 const MAX_VAL = 9
 
-const buildGetAdjacents = (heightmap) => {
+const buildGetAdjacents = (heightmap, separator = MAX_VAL) => {
   const xMax = heightmap[0].length - 1
   const yMax = heightmap.length - 1
 
   return (x, y) => [
-    y > 0 ? heightmap[y - 1][x] : MAX_VAL, // top
-    x < xMax ? heightmap[y][x + 1] : MAX_VAL, // right
-    y < yMax ? heightmap[y + 1][x] : MAX_VAL, // bottom
-    x > 0 ? heightmap[y][x - 1] : MAX_VAL, // left
+    y > 0 ? heightmap[y - 1][x] : separator, // top
+    x < xMax ? heightmap[y][x + 1] : separator, // right
+    y < yMax ? heightmap[y + 1][x] : separator, // bottom
+    x > 0 ? heightmap[y][x - 1] : separator, // left
   ]
 }
 
@@ -39,15 +40,63 @@ const partOne = (heightmap) => {
   return sum(lowpoints)
 }
 
+const DOT = '.'
+
+const partTwo = (heightmap) => {
+  const boundedHeightmap = heightmap.map((line) =>
+    line.map((v) => (v === MAX_VAL ? DOT : v))
+  )
+
+  const getAdjascents = buildGetAdjacents(heightmap, DOT)
+
+  const searchDepth = (i, j) => {
+    let count = 0
+
+    if (boundedHeightmap[j][i] !== DOT) {
+      count += 1
+      boundedHeightmap[j][i] = DOT
+      const [top, right, bottom, left] = getAdjascents(i, j)
+      if (top !== DOT) {
+        count += searchDepth(i, j - 1)
+      }
+      if (right !== DOT) {
+        count += searchDepth(i + 1, j)
+      }
+      if (bottom !== DOT) {
+        count += searchDepth(i, j + 1)
+      }
+      if (left !== DOT) {
+        count += searchDepth(i - 1, j)
+      }
+    }
+
+    return count
+  }
+
+  const sizes = []
+
+  heightmap.forEach((_line, j) => {
+    _line.forEach((_value, i) => {
+      const count = searchDepth(i, j)
+      if (count > 0) {
+        sizes.push(count)
+      }
+    })
+  })
+
+  return multiplyArray(sizes.sort((a, b) => b - a).slice(0, 3))
+}
+
 const solve = (lines) => {
   const heightmap = lines.map((line) => toNumberArray(line.split('')))
 
-  const result = partOne(heightmap)
+  let result = partOne(heightmap)
 
   console.log('> result 1:', result)
 
-  // and the second part here
-  // console.log('> result 2:')
+  result = partTwo(heightmap)
+
+  console.log('> result 2:', result)
 }
 
 export default () => {
